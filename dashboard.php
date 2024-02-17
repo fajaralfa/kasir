0f01d427914ec45e11ec5ddae9e8e7d2127eeba0
fajaralfa <11283360-fajaralfa@users.noreply.gitlab.com>
2024-02-18 06:06:35 +0700

    [dashboard] tampilkan penjualan dan pendapatan bulan ini

--- dashboard.php
+++ dashboard.php
@@ -1,6 +1,9 @@
 <?php
 
 require 'start.php';
+$sql = "SELECT COUNT(*) as bulan_ini, SUM(total_harga) AS pendapatan_bulan_ini FROM penjualan WHERE
+    MONTH(tanggal_penjualan) = MONTH(NOW()) AND YEAR(tanggal_penjualan) = YEAR(NOW())";
+$penjualan = $db->query($sql)->fetch_assoc();
 
 ?>
 
@@ -9,6 +12,29 @@ $title = 'Dashboard';
 require 'layout/header.php';
 ?>
 
-<h1>Halaman Dashboard</h1>
+<div class="container py-5">
+    <div class="row" style="width: 50rem;">
+    <div class="col">
+        <div class="card">
+            <div class="card-header">
+                <h3>Penjualan Bulan Ini</h3>
+            </div>
+            <div class="card-body">
+                <h1><?= $penjualan['bulan_ini'] ?></h1>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card">
+            <div class="card-header">
+                <h3>Pendapatan Bulan Ini</h3>
+            </div>
+            <div class="card-body">
+                <h1><?= $penjualan['pendapatan_bulan_ini'] ?></h1>
+            </div>
+        </div>
+    </div>
+    </div>
+</div>
 
 <?php require 'layout/footer.php' ?>
