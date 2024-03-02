b3860a7a925a0e10de73f7c595c9e6226bc81517
fajaralfa <11283360-fajaralfa@users.noreply.gitlab.com>
2024-03-02 15:28:39 +0700

    dokumentasi function.php (verbose)

--- system/functions.php
+++ system/functions.php
@@ -21,46 +21,51 @@
 /** membuat alamat url berdasarkan folder proyek */
 function url($url)
 {
-    /** */
+    /**
+     * $_SERVER['REQUEST_URI'] akan berisi:
+     * '/nama_folder_proyek/nama_folder/nama_file' atau '/nama_folder_proyek/nama_file'
+     */
     $req_url = $_SERVER['REQUEST_URI']; // ambil data url saat ini
-    $path = parse_url($req_url)['path']; // pisahkan data url dari data request
-    $root = explode('/', $path)[1];
-
-    $result = "/$root$url";
-    return $result;
+    $path = parse_url($req_url)['path']; // pisahkan data path dari data request
+    $data_kata = explode('/', $path); // pisahkan setiap kata dengan garis miring
+    $nama_proyek = $data_kata[1]; // ambil data kedua (ingat, data pertama diakses dengan angka 0, data kedua diakses dengan angka 1, dst)
+    $result = "/$nama_proyek$url"; // gabungkan nama_folder_proyek dengan url target
+    return $result; // keluarkan hasilnya
 }
 
-/**
- * cek metode request
- */
+/** cek metode request yang dikirim (GET, POST, PUT, PATCH, dll) */
 function request_is($method)
 {
-    return $method == $_SERVER['REQUEST_METHOD'];
+    // bandingkan apakah method dari webnya ($_SERVER['REQUEST_METHOD']) sama dengan yang diharapkan ($method)
+    return $method === $_SERVER['REQUEST_METHOD'];
 }
 
-/**
- * mengalihkan halaman
- */
+/** mengalihkan halaman */
 function redirect($url)
 {
-    header("location: $url");
-    die;
+    header("location: $url"); // atur header 'location' sesuai $url yang diminta
+    die; // lalu jangan jalankan apapun lagi
 }
 
 /** kembali ke halaman sebelumnya */
 function redirect_back()
 {
-    $last_url = session_get('last_url', '');
-    $current_url = $_SERVER['REQUEST_URI'];
-    if (! $last_url && $current_url !== url('/login.php')) {
+    $last_url = session_get('last_url', null); // ambil data url sebelumnya, jika tidak ada maka kosongkan
+    $current_url = $_SERVER['REQUEST_URI']; // ambil data url saat ini
+
+    // cek jika tidak ada url sebelumnya, dan url saat ini bukan halaman login, maka alihkan ke halaman login
+    if (is_null($last_url) && $current_url !== url('/login.php')) {
         redirect(url('/login.php'));
     } else {
+        // jika ada maka pindah ke halaman sebelumnya
         redirect($last_url);
     }
 }
 
 /**
- * membuat / mengedit nilai session
+ * atur data session dengan kata kunci $key menjadi $value,
+ * contoh:
+ * atur data user di session, menjadi data yang berisi nama user, username, level, dll
  */
 function session_set($key, $value)
 {
@@ -68,7 +73,9 @@ function session_set($key, $value)
 }
 
 /**
- * mengambil nilai session
+ * mengambil data dengan kata kunci $key dari dari session
+ * contoh:
+ * ambil data user dari session, jika datanya tidak ada maka berikan nilai bawaan
  */
 function session_get($key, $default = [])
 {
@@ -76,7 +83,7 @@ function session_get($key, $default = [])
 }
 
 /**
- * menghapus nilai session
+ * menghapus nilai session (mengosongkan)
  */
 function session_remove($key)
 {
@@ -84,16 +91,9 @@ function session_remove($key)
 }
 
 /**
- * menghapus nilai flash session
- */
-function session_unflash()
-{
-    session_remove('errors');
-    session_remove('messages');
-}
-
-/**
  * membuat pesan errors
+ * pesan ini hanya akan muncul sekali,
+ * jadi jika halamannya direfresh lagi maka pesannya tidak ada
  */
 function flash_errors($texts)
 {
@@ -102,6 +102,8 @@ function flash_errors($texts)
 
 /**
  * membuat pesan
+ * pesan ini hanya akan muncul sekali,
+ * jadi jika halamannya direfresh lagi maka pesannya tidak ada
  */
 function flash_messages($texts)
 {
@@ -109,7 +111,28 @@ function flash_messages($texts)
 }
 
 /**
- * 
+ * menghapus nilai flash session
+ * function ini berfungsi untuk mengosongkan pesan dan pesan error.
+ * function ini hanya dipakai sekali, yaitu di layout/footer.php.
+ * kenapa dibagian footer? karena jika misalkan di header atau di bagian body,
+ * kemungkinan pesannya akan terhapus sebelum muncul,
+ * jadi lebih baik disimpan di bagian bawah (footer.php)
+ * agar pesannya masih sempat muncul sebelum dihapus
+ */
+function session_unflash()
+{
+    session_remove('errors');
+    session_remove('messages');
+}
+
+/**
+ * mengembalikan selected, jika $value1 dan $value2 sesuai
+ * contoh:
+ * $value1 adalah nilai level petugas dari database
+ * $value2 adalah nilai level petugas yang diinginkan
+ * jika level di $value1 dan $value2 memiliki nilai sama (misalkan nilainya 'admin'),
+ * maka function ini akan mengeluarkan teks 'selected'
+ * function ini dipakai di dalam elemen option html
  */
 function option_selected($value1, $value2)
 {
@@ -117,18 +140,24 @@ function option_selected($value1, $value2)
 }
 
 /**
- * 
+ * mengembalikan teks selected, jika url yang diakses memiliki awalan url yang diminta
+ * contoh: 
+ * url yang diminta = '/keranjang'
+ * url yang diakses = '/keranjang/index.php'
  */
-
 function bs_active($url)
 {
-    $req_url = $_SERVER['REQUEST_URI'];
+    $req_url = $_SERVER['REQUEST_URI']; // ambil data url
+
+    // cek apakah url yang diakses memiliki awalan url yang diminta
     return strpos($req_url, url($url)) === 0 ? 'active' : '';
 }
 
-/** memformat nilai uang */
+/**
+ * memformat angka dengan format rupiah
+*/
 function rp($nilai_uang)
 {
-    $angka = number_format($nilai_uang, 0, ',', '.');
-    return "Rp. $angka,-";
+    $angka = number_format($nilai_uang, 0, null, '.');
+    return "Rp. $angka,-"; // contoh Rp. 10.000,-
 }
