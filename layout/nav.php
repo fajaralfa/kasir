ce1a34449dddd7f5459bf921bcacdffad86715a4
fajaralfa <11283360-fajaralfa@users.noreply.gitlab.com>
2024-03-02 14:38:57 +0700

    [nav] tombol yang muncul sesuai level user

--- layout/nav.php
+++ layout/nav.php
@@ -3,17 +3,18 @@
         Dashboard
     </a>
     <a href="<?= uri('/penjualan/') ?>" class="btn btn-primary <?= bs_active('/penjualan/') ?>">
-        Penjualan
+        Riwayat Penjualan
     </a>
     <a href="<?= uri('/produk/') ?>" class="btn btn-primary <?= bs_active('/produk/') ?>">
         Produk
     </a>
-    <a href="<?= uri('/keranjang/') ?>" class="btn btn-primary <?= bs_active('/keranjang/') ?>">
-        Keranjang (<?= count(session_get('keranjang')) ?>)
-    </a>
     <?php if ($user['level'] == 'admin') : ?>
         <a href="<?= uri('/petugas/') ?>" class="btn btn-primary <?= bs_active('/petugas/') ?>">
             Petugas
+        </a>
+    <?php elseif ($user['level'] == 'petugas') : ?>
+        <a href="<?= uri('/keranjang/') ?>" class="btn btn-primary <?= bs_active('/keranjang/') ?>">
+            Keranjang (<?= count(session_get('keranjang')) ?>)
         </a>
     <?php endif ?>
     <div class="fw-bold text-white ms-auto px-2"><?= "{$user['nama']} | {$user['level']}" ?></div>
