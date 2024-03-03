d6dac0f4054fe9910fce3a9e03878b3fa163deea
fajaralfa <fajaralfrzi@gmail.com>
2024-03-03 14:44:24 +0700

    jumlah barang yang dibeli tidak boleh melebihi stok

--- produk/ke_keranjang.php
+++ produk/ke_keranjang.php
@@ -7,9 +7,15 @@ if (request_is('POST')) {
     $produk_id = $_POST['produk_id'];
     $nama = $_POST['nama'];
     $harga = (int) $_POST['harga'];
+    $stok = (int) $_POST['stok'];
     $jumlah = (int) $_POST['jumlah'];
 
-    keranjang_insert($produk_id, $nama, $harga, $jumlah);
-    flash_messages(['Produk berhasil ditambahkan ke keranjang!']);
+    if ($stok >= $jumlah) {
+        keranjang_insert($produk_id, $nama, $harga, $jumlah);
+        flash_messages(['Produk berhasil ditambahkan ke keranjang!']);
+    } else {
+        flash_errors(['Stok kurang!']);
+    }
+
     redirect('index.php');
 }
